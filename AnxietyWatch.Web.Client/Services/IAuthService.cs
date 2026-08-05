2274cb23ebd27cfca92211f08690fd6c0fd00956
diana <diana@5CG9033ZSN>
2026-08-05 01:44:33 -0600

    cambios, bugs

--- AnxietyWatch.Web.Client/Services/IAuthService.cs
+++ AnxietyWatch.Web.Client/Services/IAuthService.cs
@@ -6,5 +6,5 @@ public interface IAuthService
 {
     Task<AuthResponse> LoginAsync(LoginRequest request);
     Task<AuthResponse> RegisterAsync(RegisterRequest request);
-    Task LogoutAsync();
+    Task LogoutAsync(); 
 }
